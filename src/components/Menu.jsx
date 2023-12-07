da7c9b83d9a9c1522a5da8327dcbe9cd7f5e55ff
Kebalepile <pimpabutterfly86@gmail.com>
2023-12-07 21:08:24 +0200

    enable toggle for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,26 @@
-import { HiMenuAlt3 } from "react-icons/hi";
+import { useState } from "react";
+import { HiMenuAlt3, HiOutlineXCircle } from "react-icons/hi";
+
 export default function Menu() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-      <button className="menu">
-        <HiMenuAlt3 className="menu-icon" />
+      <button className="menu" onClick={toggleMenu}>
+        {menuOpen ? (
+          <HiOutlineXCircle
+            className="menu-icon"
+            style={{ width: "40px", height: "40px" }}
+          />
+        ) : (
+          <HiMenuAlt3 className="menu-icon" />
+        )}
       </button>
-      <article id="menu-details">
+      <article id="menu-details" className={menuOpen ? "open" : ""}>
         <ul>
           <li>
             <a href="#home">Home</a>
