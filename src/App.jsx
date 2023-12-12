c18eefd5d8514965f5eee0e64b1a9f80854fb1da
Kebalepile <pimpabutterfly86@gmail.com>
2023-12-12 22:57:09 +0200

    component styling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react";
 import "./App.css";
 import Main from "./components/Main";
 
 function App() {
   return (
-    <>
+    <React.Suspense fallback={<div className="loadingDiv"></div>}>
       <Main />
-    </>
+    </React.Suspense>
   );
 }
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import  profilePic from "../assets/img/pp.png";
 
 export default function Banner (){
     return (
-        <section id="home" className="banner card">
+        <section id="home" className="banner">
             <img src={profilePic} alt="profile picture" loading="lazy" id="profile-pic"/>
         </section>
     )
